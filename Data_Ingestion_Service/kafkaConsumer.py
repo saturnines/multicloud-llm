6d8872112022138611223bdfe39aade6cb230836
Kevin <kevisos123@gmail.com>
2024-09-10 09:23:02 -0500

    Added timeout, added comments.
    TODO/
    Add logging

--- Data_Ingestion_Service/kafkaConsumer.py
+++ Data_Ingestion_Service/kafkaConsumer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
 import json
-
 from kafka.errors import KafkaError
+
 from Data_Ingestion_Service import kafkaProduer
 
 consumer = KafkaConsumer(
@@ -10,25 +10,30 @@ consumer = KafkaConsumer(
     auto_offset_reset='earliest',
     group_id='foo_group',
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-    enable_auto_commit=True  # offset for committing
+    enable_auto_commit=True,
+    consumer_timeout_ms=1000  # One second timeout.
 )
-
 
 def get_next_message():
     try:
         print("Consuming messages from broker")
-        for message in consumer:
+        message = next(consumer, None)
+        if message:
             print(f"Message from partition {message.partition}, {message.offset}")
             print(f"Message value:{message.value}")
-            return message.value # This is the actual query
+            return message.value
+        else:
+            print("No message received within timeout period")
+            return None
     except KafkaError as e:
         print(f"Caught a KafkaException:{e}")
-    finally:
-        consumer.close()
-
+    except StopIteration:
+        print("No more messages to consume")
+        return None
 
 def consume_and_trigger():
-    """Consumes messages from Kafka and triggers send_task to fill the data (This is an easy way to update data too)"""
+    """Consumes messages from Kafka and triggers send_task to fill the data (This is an easy way to update data too)
+    deprecated went a different route instead of integrating this with the api call itself, I went to integrate it with the deco """
     try:
         no_message_count = 0  # counter to check,
         print("Consuming messages from broker...")
@@ -47,7 +52,6 @@ def consume_and_trigger():
     finally:
         consumer.close()
 
-# REPLACE ALL PRINT MESSAGES WITH LOGGING SHIT
 
 if __name__ == "__main__":
     consume_and_trigger()
