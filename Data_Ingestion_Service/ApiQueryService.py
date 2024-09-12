276954c82a7516c1d854151e27c1454a4a81f8f2
Kevin <kevisos123@gmail.com>
2024-09-12 18:12:49 -0500

    Cleaned up Files
    TODO//
    Add logging
    Testing

--- Data_Ingestion_Service/ApiQueryService.py
+++ Data_Ingestion_Service/ApiQueryService.py
@@ -99,4 +99,4 @@ async def get_item():
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-# Theresa  bug where the first api call is always None (lol)
+# Theresa  bug where the first api call is always None (lol
