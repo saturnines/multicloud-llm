07f88a52ee10093790db30913586d049a9159d9f
Kevin <kevisos123@gmail.com>
2025-01-29 12:25:38 -0600

    Added docs strings, organized py
    //TODO change
    port

--- LLM_Service/FuzzySearch.py
+++ LLM_Service/FuzzySearch.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import re
 app = FastAPI()
-
+from blacklist import BLACKLIST
 
 # may need to change this depending on how llm is
 class FuzzyResponse(BaseModel):
@@ -24,45 +24,12 @@ async def fuzzy_endpoint(query: str):
     return {"match": result}
 
 def fuzzy_match(query: list):
-    #  words blacklist (lots of false positives)
-    BLACKLIST = {
-        # verbs
-        "is", "am", "are", "was", "were", "be", "been", "being",
-        "have", "has", "had", "do", "does", "did", "doing",
-        "using", "making", "getting", "running", "going",
+    """
+    Match a list of words against QueryData using fuzzy matching and a sliding window.
 
-        # prepositions
-        "the", "a", "an", "and", "but", "or", "in", "on", "at", "to", "for",
-        "of", "with", "by", "from", "up", "down", "into", "onto", "upon",
-
-        # Numbers (May need to adjust since some stuff has numbers)
-        "one", "two", "three", "four", "five", "day", "week", "hour", "minute",
-        "first", "second", "third", "last", "next",
-
-        # pronouns
-        "i", "you", "he", "she", "it", "we", "they", "me", "him", "her", "us", "them",
-        "my", "your", "his", "her", "its", "our", "their",
-
-        # internet common chat xd
-        "rn", "ngl", "tbh", "fr", "lol", "lmao", "yeah", "nah", "Im", "im",
-
-        # trading specific filler
-        "price", "profit", "selling", "buying", "trade", "trading", "cost", "worth",
-        "market", "deal", "cheap", "expensive", "method", "way", "strategy",
-
-        # quantities
-        "some", "many", "much", "few", "lot", "lots", "tons", "bit",
-
-        # common adjectives
-        "good", "bad", "best", "worst", "better", "worse", "nice", "great",
-        "awesome", "cool", "real", "fake", "true", "false", "special", "secret",
-
-        # question words
-        "what", "when", "where", "who", "why", "how", "which",
-
-        # time related
-        "now", "later", "soon", "today", "tonight", "tomorrow", "yesterday",
-    }
+    Returns:
+        str: Best matched item name or None if no match found
+    """
 
     #  filter blacklist stuff
     query = [word for word in query if word and word.lower() not in BLACKLIST]
