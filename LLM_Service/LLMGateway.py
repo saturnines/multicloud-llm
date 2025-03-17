497e8093a4b5cf7a43fa021c717a796dc3c06c86
Kevin <kevisos123@gmail.com>
2025-03-16 19:02:13 -0500

    Added bridge driver connected all services to kafka network
    added more listeneres for kafka

--- LLM_Service/LLMGateway.py
+++ LLM_Service/LLMGateway.py
@@ -382,4 +382,4 @@ async def process_query(request: QueryRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8004)
